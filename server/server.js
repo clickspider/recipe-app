f14960a204f4eb867af67604ad5aa0e4632315f5
clickspider <danielfrey101@gmail.com>
2020-03-05 11:05:04 +0100

    server.js(Update receips - Fake API)

--- server/server.js
+++ server/server.js
@@ -2,29 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
-const axios = require("axios");
 
 app.use(cors());
-
-app.get("/jokes", async (req, res) => {
-  try {
-    const api_url = "http://api.icndb.com/jokes/random/10";
-    const fetch_res = await axios.get(api_url);
-    res.send(fetch_res.data);
-  } catch (err) {
-    res.send(err);
-  }
-});
 
 app.get("/", (req, res) => {
   const favRecipes = [
     {
       id: 1,
       label: "Cowboy Rib-Eye Steak",
-      date: " dd‐MM‐yyyy HH:mm",
+      date: "05‐03‐2020 11:03",
       image:
         "https://www.edamam.com/web-img/69a/69a67f4147db9f27fd295be0c2ab06f8.jpg",
-      ingredientLines: [
+      ingredient: [
         "1 3-pound bone-in rib-eye steak",
         "1 tablespoon cumin seed",
         "1 tablespoon coriander seed",
@@ -35,59 +24,40 @@ app.get("/", (req, res) => {
         "2 tablespoons finely chopped oregano",
         "5 cloves garlic, minced"
       ],
-      ingredients: [
-        {
-          text: "1 3-pound bone-in rib-eye steak",
-          weight: 1360.77711
-        },
-        {
-          text: "1 tablespoon cumin seed",
-          weight: 6.0
-        },
-        {
-          text: "1 tablespoon coriander seed",
-          weight: 5.0
-        },
-        {
-          text: "2 teaspoons black peppercorns",
-          weight: 5.8
-        },
-        {
-          text: "2 tablespoons coarse salt",
-          weight: 29.124999999507587
-        },
-        {
-          text: "1 tablespoon sweet paprika",
-          weight: 6.8
-        },
-        {
-          text: "2 tablespoons light brown sugar",
-          weight: 18.12499999969356
-        },
-        {
-          text: "2 tablespoons finely chopped oregano",
-          weight: 11.999999999797117
-        },
-        {
-          text: "5 cloves garlic, minced",
-          weight: 15.0
-        }
+      vegetarian: false,
+      url: "https://www.marthastewart.com/867361/cowboy-ribeye-steak",
+      numOfPeople: 1
+    },
+
+    {
+      id: 2,
+      label: "Individual vegetarian lasagnes",
+      date: "05‐03‐2020 13:00",
+      image:
+        "https://www.edamam.com/web-img/775/7757b08b70371bcb03d3c10199e70e77.jpg",
+      ingredient: [
+        "1.2 kg cherry tomatoes",
+        "5 sprigs of fresh thyme",
+        "extra virgin olive oil",
+        "2 shallots",
+        "2 cloves of garlic",
+        "500 g baby spinach",
+        "8-12 fresh or dried lasagne sheets",
+        "350 g ricotta cheese",
+        "WHITE SAUCE",
+        "600 ml milk",
+        "25 g unsalted butter",
+        "2 heaped tablespoons flour",
+        "150 g vegetarian sharp, mature cheese",
+        "100 g mozzarella"
       ],
-      vegetarian: false
+      vegetarian: true,
+      url: "https://www.jamieoliver.com/recipes/vegetables-recipes/",
+      numOfPeople: 1
     }
   ];
 
   res.send(favRecipes);
-});
-
-app.get("/joke", async (req, res) => {
-  try {
-    const api_url = "http://api.icndb.com/jokes/random/1";
-    const fetch_res = await axios.get(api_url);
-    res.send(fetch_res.data);
-  } catch (err) {
-    res.send(err);
-  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
