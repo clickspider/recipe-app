fe18097687463d8f39e43b1f2e2653ea103f07ac
clickspider <danielfrey101@gmail.com>
2020-04-29 16:42:29 +0200

    dist(29/04/2020 - liking system)

---
+++ dist/precache-manifest.9552bd91a43306dcdbb8c9f91a90f58e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c175b5ab422f0311bb",
+    "url": "/css/about.a79c6e81.css"
+  },
+  {
+    "revision": "b6f5449bce9203dc8d7c",
+    "url": "/css/app.4b45b2ca.css"
+  },
+  {
+    "revision": "6278f12c393427d992b7",
+    "url": "/css/chunk-vendors.1a1ac075.css"
+  },
+  {
+    "revision": "408ddd8502b934bdaa1f2cfdca11be55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c175b5ab422f0311bb",
+    "url": "/js/about.2344eafe.js"
+  },
+  {
+    "revision": "b6f5449bce9203dc8d7c",
+    "url": "/js/app.bf74bfa5.js"
+  },
+  {
+    "revision": "6278f12c393427d992b7",
+    "url": "/js/chunk-vendors.b08edb9d.js"
+  },
+  {
+    "revision": "2e97ab47ee023ad9d9ef538fe3401491",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
